76522e5ef0b8ee15bcd184977060d1b7d4a86048
EtienneLicheron <etienne.licheron@epitech.eu>
2024-06-29 17:40:50 +0100

    Added check for mail submission

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -13,6 +13,9 @@ const resend = new Resend(process.env.RESEND_API);
 export async function POST(req: any) {
     try {
         const payload: Payload = await req.json();
+        if (!payload.name || !payload.email || !payload.subject || !payload.message || payload.message.length > 300) {
+            return Response.json({ error: 'Missing required fields' }, { status: 400 });
+        }
         const { data, error } = await resend.emails.send({
             from: 'Portfolio <onboarding@resend.dev>',
             to: ['etienne.licheron@epitech.eu'],
